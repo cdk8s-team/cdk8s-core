ff112b04888ab64fcd720131484f61f8bf6ff8a1
iliapolo <epolon@amazon.com>
2021-10-04 01:47:27 +0300

    setup script
    Signed-off-by: iliapolo <epolon@amazon.com>

--- git-hooks/setup.sh
+++ git-hooks/setup.sh
@@ -1,2 +1,10 @@
-# Git Hooks
+#!/bin/bash
 
+##############################################
+# Setup shared .git hooks for this project.
+#
+
+hooksdir="$(cd $(dirname $0) && pwd)"
+
+git config core.hooksPath ${hooksdir}
+echo "Configured core.hooksPath to ${hooksdir}"
