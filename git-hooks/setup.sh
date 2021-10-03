2b08f78b33ae48aa1650781b9dfa3966e2c58fc2
iliapolo <epolon@amazon.com>
2021-10-04 01:33:32 +0300

    asd
    Signed-off-by: iliapolo <epolon@amazon.com>

---
+++ git-hooks/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+###########################################
+# Setup shared .git hooks on your system.
+###########################################
+
+cp prepare-commit-msg .git/hooks/prepare-commit-msg
