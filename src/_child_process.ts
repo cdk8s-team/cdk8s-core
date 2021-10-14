a28f99d666d6131b1d1a40f59759d7bb074f69cf
iliapolo <epolon@amazon.com>
2021-10-14 13:55:01 +0300

    comment
    Signed-off-by: iliapolo <epolon@amazon.com>

--- src/_child_process.ts
+++ src/_child_process.ts
@@ -1,3 +1,8 @@
+/****************************************************************************************
+ * Expose `child_process` via our own object that can be easily patched by jest for tests.
+ * Consumers of the `child_process` module should add functions to this object and import it
+ * wherever needed.
+ */
 import { spawnSync } from 'child_process';
 
 export const _child_process = {
