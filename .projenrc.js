303fa0ed17278af0b0dc43c26c283603ab17d623
Momo Kornher <mail@moritzkornher.de>
2023-02-27 17:35:34 +0000

    chore: fix go release for 1.x
    Basically reverting the repo change that was introduced in this commit
    https://github.com/cdk8s-team/cdk8s-core/commit/30f2cf6b0aa53a166efded97bece284a2cef5218
    
    Signed-off-by: Momo Kornher <mail@moritzkornher.de>

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,7 @@ const { JsonFile, github } = require('projen');
 
 const project = new Cdk8sTeamJsiiProject({
   name: 'cdk8s',
+  repoName: 'cdk8s-core',
   description: 'This is the core library of Cloud Development Kit (CDK) for Kubernetes (cdk8s). cdk8s apps synthesize into standard Kubernetes manifests which can be applied to any Kubernetes cluster.',
   projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
   peerDeps: [
