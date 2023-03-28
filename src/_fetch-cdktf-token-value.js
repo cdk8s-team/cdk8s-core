fc74a3bd51407f3b904ceec1ea42c11acfcbb9d4
epolon <epolon@amazon.com>
2023-03-28 20:05:37 +0300

    debug logs

--- src/_fetch-cdktf-token-value.js
+++ src/_fetch-cdktf-token-value.js
@@ -5,7 +5,15 @@ const address = process.argv[2];
 const attribute = process.argv[3];
 const stackName = process.argv[4];
 
+function debug(message) {
+  if (process.env.RESOLVE_CLOUD_TOKENS_POC_DEBUG == '1') {
+    console.error(`cdk8s.resolve.cdktf | ${message}`)
+  }
+}
+
 async function fetch() {
+
+  debug(`Invoking | terraform show -json`)
 
   // https://developer.hashicorp.com/terraform/cli/commands/state/show#usage
   // TODO this command is fairly slow, this about some sort of cross process cache
@@ -18,9 +26,20 @@ async function fetch() {
 
   // TODO ensure one
   const resource = resources.filter(r => r.address === address)[0]
+  
+  const values = resource['values']
 
-  return resource['values'][attribute]
+  debug(`Found | [Address=${address}, Values=${JSON.stringify(values, null, 2)}] `)  
+
+  const value = values[attribute]
+
+  debug(`Found | ${stackName}/${address}/${attribute} => ${value}`)
+
+  return value;
+
 }
+
+debug(`Request | [address=${address}, attribute=${attribute}, stackName=${stackName}]`)
 
 fetch()
   .then((data) => {
