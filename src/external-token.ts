3a80e4860a45340949ab01a69107f713596825bc
epolon <epolon@amazon.com>
2023-04-09 09:38:44 +0300

    mid work

--- src/external-token.ts
+++ src/external-token.ts
@@ -1,4 +1,13 @@
+import { ApiObject } from './api-object';
+
+export class ResolutionContext {
+
+  public readonly scope: ApiObject;
+  public readonly key: string[];
+
+}
+
 export interface ITokenResolver {
 
-  resolve(value: any): any;
+  resolve(context: ResolutionContext, value: any): any;
 }
